d66b7c5efc072f60e624348f18679dcf85d1bdd2
bartlomiejszwarc <szwarc.it@gmail.com>
2024-04-25 15:27:25 +0200

    Added routerlink to category box

--- src/app/components/home/category-box/category-box.component.ts
+++ src/app/components/home/category-box/category-box.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { categories } from '../../../../assets/categories/categories';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category-box',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './category-box.component.html',
   styleUrl: './category-box.component.css',
 })
